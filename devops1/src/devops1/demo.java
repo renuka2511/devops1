30fa8bad7a1f39b60d1decfca392d8121c899d2b
Moumi <Moumi@Moumitha>
2023-09-30 14:16:17 +0530

    demo

---
+++ devops1/src/devops1/demo.java
@@ -0,0 +1,11 @@
+package devops1;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is first project");
+
+	}
+
+}
